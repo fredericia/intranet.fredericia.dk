30f1f2d93655bd81f5f6a874223f15b33297058e
Morten Nissen <mn@bellcom.dk>
2015-10-22 13:43:34 +0200

    Create panels-pane default template

--- public_html/profiles/os2intra/themes/fki/templates/panel-pane/panels-pane.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/panel-pane/panels-pane.tpl.php
@@ -2,41 +2,47 @@
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>
 <?php endif; ?>
+
 <div class="<?php print $classes; ?>" <?php print $id; ?> <?php print $attributes; ?>>
-  <?php if ($admin_links): ?>
-    <?php print $admin_links; ?>
-  <?php endif; ?>
+  <div class="fki-box">
+    <?php if ($admin_links): ?>
+      <?php print $admin_links; ?>
+    <?php endif; ?>
 
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <<?php print $title_heading; ?><?php print $title_attributes; ?>>
-      <?php print $title; ?>
-    </<?php print $title_heading; ?>>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <div class="fki-box-heading">
+        <?php print $title_heading; ?><?php print $title_attributes; ?>
+        <?php print $title; ?>
+        <?php print $title_heading; ?>
+      </div>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
 
-  <?php if ($feeds): ?>
-    <div class="feed">
-      <?php print $feeds; ?>
+    <div class="fki-box-body pane-content">
+      <?php print render($content); ?>
     </div>
-  <?php endif; ?>
 
-  <div class="pane-content">
-    <?php print render($content); ?>
+    <?php if ($links): ?>
+      <div class="links">
+        <?php print $links; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($more): ?>
+      <div class="more-link">
+        <?php print $more; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($feeds): ?>
+      <div class="feed">
+        <?php print $feeds; ?>
+      </div>
+    <?php endif; ?>
   </div>
-
-  <?php if ($links): ?>
-    <div class="links">
-      <?php print $links; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($more): ?>
-    <div class="more-link">
-      <?php print $more; ?>
-    </div>
-  <?php endif; ?>
 </div>
+
 <?php if ($pane_suffix): ?>
   <?php print $pane_suffix; ?>
 <?php endif; ?>
