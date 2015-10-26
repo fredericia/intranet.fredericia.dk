2022ce856c0c418f59cc3204843750d4e78b6b40
Franz Skaaning <fs@bellcom.dk>
2015-10-26 14:52:28 +0100

    Opdateret modul pga fejl
    Der er brugt en dev udgave

--- public_html/sites/all/modules/contrib/homebox/homebox.js
+++ public_html/sites/all/modules/contrib/homebox/homebox.js
@@ -34,6 +34,7 @@
         });
 
         $homebox.find('#homebox-add-link').click(function () {
+          $(this).toggleClass('active');
           $homebox.find('#homebox-add').toggle();
         });
 
@@ -271,32 +272,6 @@
           $portletHeader.attr('style', 'background: ' + color);
           $portlet.find('.homebox-portlet-inner').attr('style', 'border: 1px solid ' + color);
           Drupal.homebox.pageChanged();
-        });
-
-        // Add tooltips to icons
-        $portlet.find('.portlet-icon').tipsy({
-          gravity: 's',
-          title: function () {
-            switch ($(this).attr('class').replace('portlet-icon portlet-', '')) {
-            case 'close':
-              return Drupal.t('Close');
-            case 'maximize':
-              return Drupal.t('Maximize');
-            case 'minimize':
-              return Drupal.t('Minimize');
-            case 'minus':
-              return Drupal.t('Collapse');
-            case 'plus':
-              return Drupal.t('Expand');
-            case 'settings':
-              return Drupal.t('Settings');
-            }
-          }
-        });
-
-        // Remove tooltips on header clicks
-        $portletHeader.click(function () {
-          $('.tipsy').remove();
         });
       });
     }
