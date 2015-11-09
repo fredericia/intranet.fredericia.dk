61fe5858fc19cb95d6d37bde6750091ce0e0ee85
Morten Nissen <mn@bellcom.dk>
2015-11-09 10:23:29 +0100

    Add proper teasers

--- public_html/profiles/os2intra/themes/fki/templates/node/teaser/node--teaser.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/node/teaser/node--teaser.tpl.php
@@ -1,11 +1,11 @@
 <?php if ($view_mode == 'teaser'): ?>
   <!-- node--teaser.tpl.php -->
   <!-- Begin - teaser -->
-  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> fki-teaser"<?php print $attributes; ?>>
+  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> fki-teaser fki-box fki-box-small-spacing"<?php print $attributes; ?>>
 
     <?php if (isset($author_full_name) and $updated_at_short): ?>
       <!-- Begin - entity info -->
-      <ul class="fki-list-info fki-entity-info">
+      <ul class="fki-teaser-info fki-entity-info">
         <li><?php print l($author_full_name, 'user/' . $node->uid); ?></li>
         <li><span><?php print t('Sidst opdateret d.'); ?> <?php print $updated_at_short; ?></span></li>
       </ul>
