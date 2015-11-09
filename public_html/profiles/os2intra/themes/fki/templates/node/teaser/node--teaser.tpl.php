4873e181f48c50856ec89e9816dba1f2d5d34562
Morten Nissen <mn@bellcom.dk>
2015-11-09 12:13:45 +0100

    Add teaser styling

--- public_html/profiles/os2intra/themes/fki/templates/node/teaser/node--teaser.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/node/teaser/node--teaser.tpl.php
@@ -2,35 +2,55 @@
   <!-- node--teaser.tpl.php -->
   <!-- Begin - teaser -->
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> fki-teaser fki-box fki-box-small-spacing"<?php print $attributes; ?>>
+    <div class="table">
+      <div class="table-row">
 
-    <?php if (isset($author_full_name) and $updated_at_short): ?>
-      <!-- Begin - entity info -->
-      <ul class="fki-teaser-info fki-entity-info">
-        <li><?php print l($author_full_name, 'user/' . $node->uid); ?></li>
-        <li><span><?php print t('Sidst opdateret d.'); ?> <?php print $updated_at_short; ?></span></li>
-      </ul>
-      <!-- End - entity info -->
-    <?php endif ?>
+        <?php if (isset($content['field_os2intra_image'])): ?>
+          <div class="table-cell fki-teaser-image-container">
 
-    <!-- Begin - heading -->
-    <div class="fki-teaser-heading">
-      <h3 class="fki-teaser-heading-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
-    </div>
-    <!-- End - heading -->
+            <!-- Begin - image -->
+            <div class="fki-teaser-image">
+              <?php print render($content['field_os2intra_image']); ?>
+            </div>
+            <!-- End - image -->
 
-    <!-- Begin - body -->
-    <div class="fki-teaser-body">
+          </div>
+        <?php endif; ?>
 
-      <?php if (isset($content['body'])): ?>
-        <!-- Begin - body -->
-        <div class="fki-teaser-body-content">
-          <?php print render($content['body']); ?>
+        <div class="table-cell">
+
+          <?php if (isset($author_full_name) and $updated_at_short): ?>
+            <!-- Begin - entity info -->
+            <ul class="fki-teaser-info fki-entity-info">
+              <li><?php print l($author_full_name, 'user/' . $node->uid); ?></li>
+              <li><span><?php print t('Sidst opdateret d.'); ?> <?php print $updated_at_short; ?></span></li>
+            </ul>
+            <!-- End - entity info -->
+          <?php endif ?>
+
+          <!-- Begin - heading -->
+          <div class="fki-teaser-heading">
+            <h3 class="fki-teaser-heading-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
+          </div>
+          <!-- End - heading -->
+
+          <!-- Begin - body -->
+          <div class="fki-teaser-body">
+
+            <?php if (isset($content['body'])): ?>
+              <!-- Begin - body -->
+              <div class="fki-teaser-body-content">
+                <?php print render($content['body']); ?>
+              </div>
+              <!-- End - body -->
+            <?php endif; ?>
+
+          </div>
+          <!-- End - body -->
+
         </div>
-        <!-- End - body -->
-      <?php endif; ?>
-
+      </div>
     </div>
-    <!-- End - body -->
 
     <!-- Begin - footer -->
     <div class="fki-teaser-footer">
