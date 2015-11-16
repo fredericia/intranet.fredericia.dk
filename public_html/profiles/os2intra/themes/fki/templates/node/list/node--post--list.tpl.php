3be47fa871f7b34077c35880e8e4041605a60a78
Morten Nissen <mn@bellcom.dk>
2015-11-16 14:00:40 +0100

    Homebox content layout

--- public_html/profiles/os2intra/themes/fki/templates/node/list/node--post--list.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/node/list/node--post--list.tpl.php
@@ -3,34 +3,13 @@
   <!-- Begin - list -->
   <section id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> fki-list fki-list-post"<?php print $attributes; ?>>
 
-    <?php if (isset($author_full_name) and $updated_at_short): ?>
-      <!-- Begin - entity info -->
-      <ul class="fki-list-info fki-entity-info">
-        <li><?php print l($author_full_name, 'user/' . $node->uid); ?></li>
-        <li><span><?php print $updated_at_ago; ?></span></li>
-      </ul>
-      <!-- End - entity info -->
-    <?php endif ?>
-
     <!-- Begin - heading -->
     <div class="fki-list-heading">
+      <span class="fki-list-heading-date"><?php print $created_at_seperated['day']['integer']; ?>/<?php print $created_at_seperated['month']['integer']; ?></span>
       <h3 class="fki-list-heading-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
+      <span class="fki-list-heading-time-ago"><?php print $created_at_ago; ?></span>
     </div>
     <!-- End - heading -->
-
-    <!-- Begin - body -->
-    <div class="fki-list-body">
-
-      <?php if (isset($content['body'])): ?>
-        <!-- Begin - body -->
-        <div class="fki-list-body-content">
-          <?php print render($content['body']); ?>
-        </div>
-        <!-- End - body -->
-      <?php endif; ?>
-
-    </div>
-    <!-- End - body -->
 
   </section>
   <!-- End - list -->
