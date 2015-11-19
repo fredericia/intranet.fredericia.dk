3c5ae2476f6cc8d8b964a38df6e1028dae20bb98
Morten Nissen <mn@bellcom.dk>
2015-11-19 11:41:30 +0100

    Add extra data on basket items

--- public_html/profiles/os2intra/themes/fki/templates/node/includeable/node--includeable.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/node/includeable/node--includeable.tpl.php
@@ -5,7 +5,12 @@
 
     <!-- Begin - heading -->
     <div class="fki-includeable-heading">
+      <span class="fki-includeable-heading-date"><?php print $created_at_seperated['day']['integer']; ?>/<?php print $created_at_seperated['month']['integer']; ?></span>
       <a href="<?php print $node_url; ?>" class="fki-includeable-heading-title"><?php print $title_shortened; ?></a>
+      <span class="fki-includeable-heading-author">
+        <span class="fki-includeable-heading-author-intro"><?php print t('written by:'); ?></span>
+        <?php print l($author_full_name, 'user/' . $node->uid, array('attributes' => array('class' => 'fki-includeable-heading-author-link'))); ?>
+      </span>
     </div>
     <!-- End - heading -->
 
