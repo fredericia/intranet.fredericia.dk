6a9a52fdff0892405f2c366d107f701ce40c808c
Morten Nissen <mn@bellcom.dk>
2015-11-20 13:57:04 +0100

    Fix error message on author_full_name

--- public_html/profiles/os2intra/themes/fki/templates/node/includeable/node--includeable.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/node/includeable/node--includeable.tpl.php
@@ -7,10 +7,14 @@
     <div class="fki-includeable-heading">
       <span class="fki-includeable-heading-date"><?php print $created_at_seperated['day']['integer']; ?>/<?php print $created_at_seperated['month']['integer']; ?></span>
       <a href="<?php print $node_url; ?>" class="fki-includeable-heading-title"><?php print $title_shortened; ?></a>
+
+      <?php if (isset($author_full_name)): ?>
       <span class="fki-includeable-heading-author">
         <span class="fki-includeable-heading-author-intro"><?php print t('written by:'); ?></span>
         <?php print l($author_full_name, 'user/' . $node->uid, array('attributes' => array('class' => 'fki-includeable-heading-author-link'))); ?>
       </span>
+      <?php endif ?>
+      
     </div>
     <!-- End - heading -->
 
