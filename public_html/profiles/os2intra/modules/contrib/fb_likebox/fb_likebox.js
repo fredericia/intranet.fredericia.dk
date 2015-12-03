2035307e0385bc450601cdce1ecbbf24d791e292
Franz Skaaning <fs@bellcom.dk>
2015-12-03 14:08:52 +0100

    Facebook likebox

--- public_html/profiles/os2intra/modules/contrib/fb_likebox/fb_likebox.js
+++ public_html/profiles/os2intra/modules/contrib/fb_likebox/fb_likebox.js
@@ -1,11 +1,21 @@
 (function($) {
 
 	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.3&appId=579264578881928";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/da_DK/sdk.js#xfbml=1&version=v2.5&appId=349377909301";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
+
+  var url = $('.fb-page').data('refresh');
+   
+  $('.fb-page').load(url, { width: $('#fb-root').width() },
+  function() {
+     FB.XFBML.parse(document.getElementById('.fb-page'),
+     function() {
+        $('.fb-page').fadeIn("slow");
+     });
+  });
 }(jQuery));
