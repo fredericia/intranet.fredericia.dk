0deaf7d28bdb580c09442a77327800e643aec1e6
Morten Nissen <mn@bellcom.dk>
2015-12-18 16:51:19 +0100

    Organisation tree

---
+++ public_html/profiles/os2intra/themes/fki/templates/node/includeable/node--os2intra_org_group_unit--includeable.tpl.php
@@ -0,0 +1,19 @@
+<?php if ($view_mode == 'includeable'): ?>
+  <!-- node--os2intra_org_group_unit--includeable.tpl.php -->
+  <!-- Begin - includeable -->
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> os2-node-includeable os2-node-includeable-organisation-unit"<?php print $attributes; ?>>
+
+    <!-- Begin - heading -->
+    <div class="os2-node-includeable-heading">
+      <a href="<?php print $node_url; ?>" class="os2-node-includeable-heading-title"><?php print $title_shortened; ?></a>
+    </div>
+    <!-- End - heading -->
+
+    <!-- Begin - users -->
+
+    <!-- End - users -->
+
+  </div>
+  <!-- End - includeable -->
+
+<?php endif; ?>
