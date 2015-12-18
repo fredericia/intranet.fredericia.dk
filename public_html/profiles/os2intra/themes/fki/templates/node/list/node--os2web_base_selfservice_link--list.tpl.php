ec46e93290e870ea692a4fed78ba0a213cc3fc9c
Morten Nissen <mn@bellcom.dk>
2015-12-18 15:52:55 +0100

    Self service links setup

--- public_html/profiles/os2intra/themes/fki/templates/node/list/node--os2web_base_selfservice_link--list.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/node/list/node--os2web_base_selfservice_link--list.tpl.php
@@ -3,9 +3,8 @@
   <!-- Begin - list -->
   <section id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> os2-node-list os2-node-list-selfservice"<?php print $attributes; ?>>
 
-    <!-- Begin - heading -->
-    <div class="os2-node-list-heading">
-      <h3 class="os2-node-list-heading-title">
+    <!-- Begin - body -->
+    <div class="os2-node-list-body">
 
         <?php if (isset($content['field_urlos2intra_selvbetje_link'])): ?>
           <!-- Begin - external link -->
@@ -17,9 +16,8 @@
           <!-- End - internal link -->
         <?php endif; ?>
 
-      </h3>
     </div>
-    <!-- End - heading -->
+    <!-- End - body -->
 
   </section>
   <!-- End - list -->
