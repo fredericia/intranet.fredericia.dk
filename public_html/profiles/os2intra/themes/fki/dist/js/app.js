6a6e5fc5143f98c03427c7440157b3cca43e27f4
Morten Nissen <mn@bellcom.dk>
2016-01-11 13:01:34 +0100

    Toggle for organisation tree

--- public_html/profiles/os2intra/themes/fki/dist/js/app.js
+++ public_html/profiles/os2intra/themes/fki/dist/js/app.js
@@ -3604,6 +3604,46 @@ var bs3Alert = (function ($) {
   return pub;
 })(jQuery);
 
+// |--------------------------------------------------------------------------
+// | OS2Toggler
+// |--------------------------------------------------------------------------
+// |
+// | This jQuery script is written by
+// | Morten Nissen
+// |
+
+var os2Toggler = (function ($) {
+  'use strict';
+  var pub = {};
+
+  /**
+   * Instantiate
+   */
+  pub.init = function (options) {
+    registerEventHandlers();
+  }
+
+  /**
+   * Register event handlers
+   */
+  function registerEventHandlers() {
+
+    // Toggle
+    $('.os2-toggler-element-toggle').on('click', function (event) {
+      event.preventDefault();
+
+      var $element = $(this);
+
+      // Toggle active class
+      $element
+        .parents('.os2-toggler-element')
+        .toggleClass('active');
+    });
+  }
+
+  return pub;
+})(jQuery);
+
 // Document ready
 (function ($) {
   'use strict';
@@ -3616,6 +3656,9 @@ var bs3Alert = (function ($) {
 
   // Enable popover button
   popoverButton.init();
+
+  // Enable OS2Toggler
+  os2Toggler.init();
 
 })(jQuery);
 
--- public_html/profiles/os2intra/themes/fki/templates/node/includeable/node--os2intra_org_group_unit--includeable.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/node/includeable/node--os2intra_org_group_unit--includeable.tpl.php
@@ -1,16 +1,16 @@
 <?php if ($view_mode == 'includeable'): ?>
   <!-- node--os2intra_org_group_unit--includeable.tpl.php -->
   <!-- Begin - includeable -->
-  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> os2-node-includeable os2-node-includeable-organisation-unit"<?php print $attributes; ?>>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> os2-node-includeable os2-node-includeable-organisation-unit os2-toggler-element"<?php print $attributes; ?>>
 
     <!-- Begin - heading -->
     <div class="os2-node-includeable-heading">
-      <a href="<?php print $node_url; ?>" class="os2-node-includeable-heading-title"><?php print $title_shortened; ?></a>
+      <a href="<?php print $node_url; ?>" class="os2-node-includeable-heading-title os2-toggler-element-toggle"><?php print $title_shortened; ?></a>
     </div>
     <!-- End - heading -->
 
     <!-- Begin - body -->
-    <div class="os2-node-includeable-body">
+    <div class="os2-node-includeable-body os2-toggler-element-content">
 
       <!-- Begin - users -->
       <?php if (!empty($users)): ?>
