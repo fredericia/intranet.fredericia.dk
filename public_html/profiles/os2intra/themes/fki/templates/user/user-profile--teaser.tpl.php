92bb08fbf7b089c0f288400e89071049f52310dd
Morten Nissen <mn@bellcom.dk>
2016-01-25 09:15:09 +0100

    293 - user display

--- public_html/profiles/os2intra/themes/fki/templates/user/user-profile--teaser.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/user/user-profile--teaser.tpl.php
@@ -1,7 +1,6 @@
 <!-- user-profile--teaser.tpl.php -->
 <!-- Begin - teaser -->
 <div id="user-profile-<?php print $account->uid; ?>" class="<?php print $classes; ?> os2-user-teaser"<?php print $attributes; ?>>
-
   <div class="table">
     <div class="table-row">
       <div class="table-cell os2-user-teaser-image">
@@ -17,9 +16,11 @@
       </div>
       <div class="table-cell os2-user-teaser-information">
 
-        <!-- Begin - full name -->
         <?php if (isset($user_full_name)): ?>
-          <?php print l($user_full_name, 'user/' . $account->uid); ?>
+          <!-- Begin - full name -->
+          <div class="os2-user-teaser-full-name">
+            <?php print l($user_full_name, 'user/' . $account->uid); ?>
+          </div>
         <?php endif ?>
         <!-- End - full name -->
 
@@ -29,6 +30,30 @@
             <?php print render($user_profile['field_os2intra_user_titles']); ?>
           </div>
           <!-- End - job title -->
+        <?php endif; ?>
+
+        <?php if (isset($user_profile['field_os2intra_phone'])): ?>
+          <!-- Begin - phone number -->
+          <div class="os2-user-teaser-phone-number">
+            <?php print render($user_profile['field_os2intra_phone']); ?>
+          </div>
+          <!-- End - phone number -->
+        <?php endif; ?>
+
+        <?php if (isset($user_profile['field_os2intra_mobile'])): ?>
+          <!-- Begin - mobile -->
+          <div class="os2-user-teaser-mobile">
+            <?php print render($user_profile['field_os2intra_mobile']); ?>
+          </div>
+          <!-- End - mobile -->
+        <?php endif; ?>
+
+        <?php if (isset($account->mail)): ?>
+          <!-- Begin - email -->
+          <div class="os2-user-teaser-email">
+            <a href="mailto:<?php print $account->mail; ?>"><?php print $account->mail; ?></a>
+          </div>
+          <!-- End - email -->
         <?php endif; ?>
 
       </div>
