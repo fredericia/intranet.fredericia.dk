b11919bea5299d1b4a97c712c3a5b2b2f89858ab
Morten Nissen <mn@bellcom.dk>
2016-06-02 14:54:29 +0200

    Homebox layout no longer blue+green

--- public_html/profiles/os2intra/themes/fki/dist/js/app.js
+++ public_html/profiles/os2intra/themes/fki/dist/js/app.js
@@ -2516,6 +2516,7 @@ var bs3Designer = (function ($) {
         }
 
         optimizeFormElements();
+        inputClear();
         appear();
         bs3Tooltip();
     }
@@ -2629,7 +2630,47 @@ var bs3Designer = (function ($) {
         var elementWidth = $element.outerWidth(true);
 
         // Remove button value and insert icon
-        $element.html($icon).css('width', elementWidth);
+        $element.html($icon).addClass('btn-loader-active').css('width', elementWidth);
+    }
+
+    /**
+     * Input clear
+     */
+    function inputClear() {
+        var $inputs = $('input[type="text"].form-control').not('.sliderfield-value-field');
+
+        // Run through all input fields and add elements to DOM
+        $inputs.each(function(index) {
+            var $input = $(this);
+            var $wrapper = $('<div />').addClass('form-control-clear-wrapper');
+            var $clearButton = $('<span />').addClass('form-control-clear').on('click touchstart', function(event) {
+                $input.attr('value', '').focus();
+
+                $(this).hide();
+            });
+
+            // Wrap input
+            $input.wrap($wrapper);
+
+            // Add clear button
+            $input.after($clearButton);
+
+            // Input has content - show clear button
+            if ($input.val().replace(/^\s+|\s+$/g, '').length > 0) {
+                $clearButton.show();
+            }
+
+            // Show clear button
+            $input.on('keyup keydown change focus', function(event) {
+
+                if ($input.val().replace(/^\s+|\s+$/g, '').length > 0) {
+
+                    if (!$clearButton.is(':visible')) {
+                        $clearButton.show();
+                    }
+                }
+            });
+        });
     }
 
     return pub;
