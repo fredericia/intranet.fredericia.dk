c17bd476161966eeb1ea18a9753e573c60f15732
Yani0029 <yani@bellcom.dk>
2016-06-20 15:04:10 +0200

    Fixed bs3-desinger init missing

--- public_html/profiles/os2intra/themes/fki/dist/js/app.js
+++ public_html/profiles/os2intra/themes/fki/dist/js/app.js
@@ -2477,25 +2477,47 @@ if (typeof jQuery === 'undefined') {
 }());
 
 // |--------------------------------------------------------------------------
-// | BS3 sidebar
+// | BS3 designer
 // |--------------------------------------------------------------------------
-// |
-// | App alike navigation with sidebar.
 // |
 // | This jQuery script is written by
 // | Morten Nissen
 // |
-var bs3Sidebar = (function ($) {
+// | - Optimize form elements
+// | - Attach footer to bottom of page on non-touch devices
+// | - Enable BS3 tooltips on non-touch devices
+// | - Disable form autocomplete on non-touch devices
+// | - Apply loader icon to .btn.btn-loader on click
+// | - Use appear on non-touch devices
+// |
+
+// jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+
+var bs3Designer = (function ($) {
     'use strict';
 
+    var Modernizr = {};
     var pub = {};
 
     /**
      * Instantiate
      */
-    pub.init = function (options) {
-        registerEventHandlers();
+    pub.init = function () {
         registerBootEventHandlers();
+        registerEventHandlers();
+    }
+
+    /**
+     * Register boot event handlers
+     */
+    function registerBootEventHandlers() {
+        if ( ! Modernizr.touch) {
+            footerAttached();
+            optimizeFormElements();
+        }
+
+        appear();
+        bs3Tooltip();
     }
 
     /**
@@ -2503,147 +2525,92 @@ var bs3Sidebar = (function ($) {
      */
     function registerEventHandlers() {
 
-        // Toggle sidebar
-        $('[data-sidebar-toggle]').on('click touchstart', function (event) {
-            event.preventDefault();
-
-            var $element = $(this);
-
-            toggleSidebar($element);
+        $(window).resize(function () {
+            footerAttached();
         });
 
-        // Toggle dropdown
-        $('.sidebar .sidebar-navigation-dropdown > a > .sidebar-navigation-dropdown-toggle').on('click touchstart', function (event) {
-            event.preventDefault();
-
-            var $element = $(this);
-
-            toggleDropdown($element);
+        $('.btn-loader').on('click', function () {
+            iconSpin($(this));
         });
     }
 
     /**
-     * Register boot event handlers
+     * Footer attached
      */
-    function registerBootEventHandlers() {
+    function footerAttached() {
+        var $footer = $('.footer');
+        var footerHeight = $footer.outerHeight(true);
+
+        if ($('body').hasClass('footer-attached')) {
+            $('.inner-wrapper').css('padding-bottom', footerHeight);
+        }
     }
 
     /**
-     * Toggle sidebar
+     * Appear
      */
-    function toggleSidebar($element) {
-        var $body = $('body');
-        var attribute = $element.attr('data-sidebar-toggle');
+    function appear() {
+        var $appear = $('.appear');
+        var $animation = $('.animation');
 
-        if (attribute != 'left' && attribute != 'right') {
+        if (Modernizr.touch || !Modernizr.cssanimations) {
+
+            $animation
+                .removeClass('animation')
+                .removeClass('animation-appear-from-top')
+                .removeClass('animation-appear-from-right')
+                .removeClass('animation-appear-from-left')
+                .removeClass('animation-appear-from-bottom')
+                .removeClass('animation-appear-from-center');
+
             return false;
         }
 
-        if (attribute == 'left' && $body.hasClass('sidebar-right-open')) {
-            $body.removeClass('sidebar-right-open');
+        // Enable appear
+        $appear.appear();
+
+        // Force processing on animation objects
+        $animation.appear({
+            force_process: true
+        });
+
+        // Animation object has appeared
+        $animation.on('appear', function () {
+
+            var $element = $(this);
+            var delay = $element.data('delay');
+
+            setTimeout(function () {
+                $element.addClass('animation-start');
+            }, delay);
+        });
+    }
+
+    /**
+     * BS tooltip
+     */
+    function bs3Tooltip() {
+        if (Modernizr.touch) {
+            $('[data-toggle=tooltip]').tooltip('hide');
+
+            return false;
         }
 
-        if (attribute == 'right' && $body.hasClass('sidebar-left-open')) {
-            $body.removeClass('sidebar-left-open');
-        }
-
-        $body.toggleClass('sidebar-' + attribute + '-open');
+        $('[data-toggle=tooltip]').tooltip();
     }
 
     /**
-     * Toggle dropdown
+     * Optimize form elements
      */
-    function toggleDropdown($element) {
-        var $parent = $element.parent().parent();
-        var parentIsActive = $parent.hasClass('active') || $parent.hasClass('active-trail') ? true : false;
-
-        if (parentIsActive) {
-            closeDropdown($parent);
-        }
-
-        else {
-            openDropdown($parent);
-        }
+    function optimizeFormElements() {
+        $('form').attr('autocomplete', 'off');
     }
 
     /**
-     * Open dropdown
+     * Icon spin
      */
-    function openDropdown($parent) {
-        var $dropdownMenu = $parent.find('> .sidebar-navigation-dropdown-menu');
-        var dropdownMenuHeight = $dropdownMenu.outerHeight(true);
-        var preAnimationCSS = { opacity: 0.1, height: 0, top: -20 };
-        var animationCSS = { opacity: 1, height: dropdownMenuHeight, top: 0 };
-        var callbackFunction = function () {
-            $dropdownMenu.attr('style', '');
-        };
-
-        closeAllDropdownMenus($parent);
-
-        $parent.addClass('active');
-
-        $dropdownMenu
-            .addClass('active')
-            .css(preAnimationCSS);
-
-        dropdownMenuAnimatedToggle($dropdownMenu, animationCSS, callbackFunction);
-    }
-
-    /**
-     * Close dropdown
-     */
-    function closeDropdown($parent) {
-        var $dropdownMenu = $parent.find('> .sidebar-navigation-dropdown-menu');
-        var animationCSS = { height: 0, opacity: 0.1 };
-        var callbackFunction = function () {
-
-            // Remove all active class' from dropdown menu and all child elements with active states
-            $dropdownMenu
-                .removeClass('active')
-                .attr('style', '')
-                .find('.active:not(a)')
-                .removeClass('active')
-                .attr('style', '');
-
-            $dropdownMenu
-                .removeClass('active-trail')
-                .attr('style', '')
-                .find('.active-trail:not(a)')
-                .removeClass('active-trail')
-                .attr('style', '');
-        };
-
-        $parent
-            .removeClass('active')
-            .removeClass('active-trail');
-
-        dropdownMenuAnimatedToggle($dropdownMenu, animationCSS, callbackFunction);
-    }
-
-    /**
-     * Close all dropdown menus
-     */
-    function closeAllDropdownMenus($parent) {
-        $parent
-            .siblings('.sidebar-navigation-dropdown.active, .sidebar-navigation-dropdown.active-trail')
-            .each(function () {
-                var $element = $(this);
-
-                closeDropdown($element);
-            });
-    }
-
-    /**
-     * Dropdown menu animated toggle
-     */
-    function dropdownMenuAnimatedToggle($dropdownMenu, animationCSS, callbackFunction) {
-        $dropdownMenu.animate(
-            animationCSS,
-            {
-                duration: 400,
-                easing  : 'easeOutSine',
-                complete: callbackFunction
-            });
+    function iconSpin($element) {
+        $element.find('.icon').addClass('icon-spin');
     }
 
     return pub;
@@ -2661,6 +2628,7 @@ var bs3Sidebar = (function ($) {
 var bs3Sidebar = (function ($) {
   'use strict';
 
+  var Modernizr = {};
   var pub = {};
 
   /**
@@ -2677,7 +2645,7 @@ var bs3Sidebar = (function ($) {
   function registerEventHandlers() {
 
     // Toggle sidebar
-    $('[data-sidebar-toggle]').on('click touchstart', function (event) {
+    $('[data-sidebar-toggle]').on('click', function (event) {
       event.preventDefault();
 
       var $element = $(this);
@@ -2686,7 +2654,7 @@ var bs3Sidebar = (function ($) {
     });
 
     // Toggle dropdown
-    $('.sidebar .sidebar-navigation-dropdown > a > .sidebar-navigation-dropdown-toggle').on('click touchstart', function (event) {
+    $('.sidebar .sidebar-navigation-dropdown > a > .sidebar-navigation-dropdown-toggle').on('click', function (event) {
       event.preventDefault();
 
       var $element = $(this);
