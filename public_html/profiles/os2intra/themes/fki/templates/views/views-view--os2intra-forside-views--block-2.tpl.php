f67975b242d72862e9f5a43808a790a3f313aa64
Simon L. Lange <sl@bellcom.dk>
2023-04-13 10:47:30 +0000

    Template test

--- public_html/profiles/os2intra/themes/fki/templates/views/views-view--os2intra-forside-views--block-2.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/views/views-view--os2intra-forside-views--block-2.tpl.php
@@ -1,8 +1,17 @@
-<div class="view-content">
+<div class="custom-view-block">
   <?php foreach ($rows as $row): ?>
-    <div class="my-wrapper">
-      <?php print render($row['title']); ?>
-      <?php print render($row['body']); ?>
+    <div class="custom-view-row">
+      <div class="custom-view-image">
+        <?php print render($row->field_field_os2intra_images); ?>
+      </div>
+      <div class="custom-view-text">
+        <div class="custom-view-title">
+          <?php print render($row->title); ?>
+        </div>
+        <div class="custom-view-body">
+          <?php print render($row->body); ?>
+        </div>
+      </div>
     </div>
   <?php endforeach; ?>
 </div>
