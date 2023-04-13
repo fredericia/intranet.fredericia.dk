b9e7c47fb3f8da200c40c09699d2489372c0ca84
Simon L. Lange <sl@bellcom.dk>
2023-04-13 09:45:59 +0000

    Added template for faellesnyt view

---
+++ public_html/profiles/os2intra/themes/fki/templates/views/views-view--os2intra-forside-views--block-2.tpl.php
@@ -0,0 +1,8 @@
+<div class="views-row">
+  <div class="views-field views-field-title">
+    {{ fields.title.content }}
+  </div>
+  <div class="views-field views-field-body">
+    {{ fields.body.content }}
+  </div>
+</div>
