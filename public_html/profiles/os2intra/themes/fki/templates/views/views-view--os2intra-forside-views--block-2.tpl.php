2bbc01bdb87634f51cb530ce57024dbfb074eff1
Simon L. Lange <sl@bellcom.dk>
2023-04-13 11:05:07 +0000

    Added correct template for view

---
+++ public_html/profiles/os2intra/themes/fki/templates/views/views-view--os2intra-forside-views--block-2.tpl.php
@@ -0,0 +1,13 @@
+<?php print $fields['field_os2intra_images']->content; ?>
+
+<div class="views-field views-field-wrapper">
+
+  <div class="views-field-title-wrapper">
+    <?php print $fields['title']->content; ?>
+  </div>
+  
+  <div class="views-field-body-wrapper">
+    <?php print $fields['body']->content; ?>
+  </div>
+
+</div>
