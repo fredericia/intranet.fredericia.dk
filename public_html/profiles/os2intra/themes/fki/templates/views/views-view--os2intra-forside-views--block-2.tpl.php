9de6d0941d15772475d3312a594473a52a1036ee
Simon L. Lange <sl@bellcom.dk>
2023-04-13 10:50:07 +0000

    Template for faellesnyt

--- public_html/profiles/os2intra/themes/fki/templates/views/views-view--os2intra-forside-views--block-2.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/views/views-view--os2intra-forside-views--block-2.tpl.php
@@ -1,17 +1,15 @@
 <div class="custom-view-block">
-  <?php foreach ($rows as $row): ?>
     <div class="custom-view-row">
       <div class="custom-view-image">
-        <?php print render($row->field_field_os2intra_images); ?>
+        <?php print render($field_field_os2intra_images); ?>
       </div>
       <div class="custom-view-text">
         <div class="custom-view-title">
-          <?php print render($row->title); ?>
+          <?php print render($title); ?>
         </div>
         <div class="custom-view-body">
-          <?php print render($row->body); ?>
+          <?php print render($body); ?>
         </div>
       </div>
     </div>
-  <?php endforeach; ?>
 </div>
