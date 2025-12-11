5a676efbf079f3edb6607e885c0c6f2d452568be
LAPTOP-RSOLLPLO\sl <sl@bellcom.dk>
2025-12-11 13:14:38 +0100

    Ændring af Search url

--- public_html/profiles/os2intra/themes/fki/templates/page.tpl.php
+++ public_html/profiles/os2intra/themes/fki/templates/page.tpl.php
@@ -87,7 +87,7 @@
           <?php if (isset($find_colleague_block)): ?>
             <!-- Begin - find colleague -->
           <div class="search-btn-wrapper">
-            <a href="/search/node" class="search-btn">
+            <a href="/search" class="search-btn">
               Søg <i class="fa fa-search"></i>
             </a>
           </div>
